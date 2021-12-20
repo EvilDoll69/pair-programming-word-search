25b9911ed4f354f0eaf6717269f0fb424ed436ed
EvilDoll69 <tetiana.fediushyn@gmail.com>
2021-12-20 20:18:33 +0000

    Function wordSearch

--- wordsearch.js
+++ wordsearch.js
@@ -3,6 +3,35 @@ const wordSearch = (letters, word) => {
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+
+    const verticalJoin = transpose(letters).map(ls => ls.join(''))
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true
+    }
+
+    return false;
 }
+
+
+const transpose = letters => {
+    const transposedArray = [];
+
+    if (letters.length === 0 ) {
+        return letters;
+    }
+
+    for (let i = 0; i < letters[0].length; i++) {
+        transposedArray.push([]);
+    }
+
+    for (let y = 0; y < letters.length; y++) {
+        for (let x = 0; x < letters[y].length; x++) {
+            transposedArray[x][y] = letters[y][x];
+        }
+    }
+
+    return transposedArray;
+};
+
 
 module.exports = wordSearch
